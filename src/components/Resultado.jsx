b8760b347ca7f27e5d6059ade6bdcc8fd466224c
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-09-12 00:58:46 -0400

    We used useCallback and useRef to freeze the displayed data, preventing it from changing due to the refresh state.

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,12 +1,20 @@
+import { useCallback, useMemo, useRef } from "react"
 import useCotizador from "../hooks/useCotizador"
 import { MARCAS, PLANES, YEARS } from "../constants"
 
 export default function Resultado() {
     const { resultado, datos } = useCotizador()
     const { marca, plan, year } = datos
+    const yearRef = useRef(year)
 
-    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
-    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
+    const [nombreMarca] = useCallback(
+        MARCAS.filter(m => m.id === Number(marca)),
+        [resultado]
+    )
+    const [nombrePlan] = useCallback(
+        PLANES.filter(p => p.id === Number(plan)),
+        [resultado]
+    )
 
     if (resultado === 0) return null
 
@@ -23,7 +31,7 @@ export default function Resultado() {
             </p>
             <p className="my-2">
                 <span className="font-bold">Año del Auto: </span>
-                {year}
+                {yearRef.current}
             </p>
             <p className="my-2 text-2xl">
                 <span className="font-bold">Total Cotización: </span>
